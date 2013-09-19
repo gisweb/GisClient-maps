8511e26ad9649791f9e46abdb1c7bbc328a75ffe
Roberto Starnini <roberto.starnini@gmail.com>
2013-09-19 08:19:18 +0200

    rename resources in ext-resources

--- demo/index.php
+++ demo/index.php
@@ -9,26 +9,26 @@ $objMapset = new gcMap($_REQUEST["mapset"]);
     <head>
         <title><?php echo $objMapset->mapOptions["title"]?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<script type="text/javascript" src="../resources/jslib/ext-3.4.0/adapter/ext/ext-base.js" ></script>
-		<script type="text/javascript" src="../resources/jslib/ext-3.4.0/ext-all.js" ></script>
-		<link type="text/css" rel="stylesheet" href="../resources/jslib/ext-3.4.0/resources/css/ext-all.css" />
+		<script type="text/javascript" src="../ext-resources/jslib/ext-3.4.0/adapter/ext/ext-base.js" ></script>
+		<script type="text/javascript" src="../ext-resources/jslib/ext-3.4.0/ext-all.js" ></script>
+		<link type="text/css" rel="stylesheet" href="../ext-resources/jslib/ext-3.4.0/ext-resources/css/ext-all.css" />
 	   <!--
-		<script type="text/javascript" src="../resources/jslib/ext-3.3.1/adapter/ext/ext-base-debug.js" ></script>
-        <script type="text/javascript" src="../resources/jslib/ext-3.3.1/ext-all-debug.js" ></script>
+		<script type="text/javascript" src="../ext-resources/jslib/ext-3.3.1/adapter/ext/ext-base-debug.js" ></script>
+        <script type="text/javascript" src="../ext-resources/jslib/ext-3.3.1/ext-all-debug.js" ></script>
 		-->
 
 		
-		<script type="text/javascript" src="../resources/jslib/openlayers/OpenLayers.js" ></script>
-		<script type="text/javascript" src="../resources/jslib/gcOloverride.js"></script>
-		<!--<script src="../resources/jslib/proj4js-compressed.js" type="text/javascript"></script>-->
+		<script type="text/javascript" src="../ext-resources/jslib/openlayers/OpenLayers.js" ></script>
+		<script type="text/javascript" src="../ext-resources/jslib/gcOloverride.js"></script>
+		<!--<script src="../ext-resources/jslib/proj4js-compressed.js" type="text/javascript"></script>-->
 
 		
-		<script type="text/javascript" src="../resources/jslib/geoext/GeoExt.js"></script>
-		<script type="text/javascript" src="../resources/jslib/TreeNodeAutoDisable.js"></script>
-		<script type="text/javascript" src="../resources/jslib/LayerTreeBuilder.js"></script>
+		<script type="text/javascript" src="../ext-resources/jslib/geoext/GeoExt.js"></script>
+		<script type="text/javascript" src="../ext-resources/jslib/TreeNodeAutoDisable.js"></script>
+		<script type="text/javascript" src="../ext-resources/jslib/LayerTreeBuilder.js"></script>
 		
-		<link type="text/css"  rel="stylesheet" href="../resources/css/gcGeoext.css" />	
-		<link type="text/css" rel="stylesheet" href="../resources/css/LayerTreeBuilder.css" />		
+		<link type="text/css"  rel="stylesheet" href="../ext-resources/css/gcGeoext.css" />	
+		<link type="text/css" rel="stylesheet" href="../ext-resources/css/LayerTreeBuilder.css" />		
 
 		<!--CARICA LA LISTA DEI PROVIDERS ESTERNI CON LE CHIAVI CORRETTE-->
 		<?php echo $objMapset->getMapProviders()?>
@@ -36,18 +36,18 @@ $objMapset = new gcMap($_REQUEST["mapset"]);
 		<!-- SCRIPT GISCLIENT -->
 		<!--<script type="text/javascript">Ext.namespace("GisClient");</script>-->
 		
-		<script src="../resources/jslib/gcMapPanel.js" type="text/javascript"></script>
-		<script src="../resources/jslib/gcTreePanel.js" type="text/javascript"></script>
-		<script src="../resources/jslib/gcSLDSelect.js" type="text/javascript"></script>
-		<script src="../resources/jslib/gcZoomToolbar.js" type="text/javascript"></script>	
+		<script src="../ext-resources/jslib/gcMapPanel.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/gcTreePanel.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/gcSLDSelect.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/gcZoomToolbar.js" type="text/javascript"></script>	
 
 		<!-- PLUGINS GISCLIENT -->		
-		<script src="../resources/jslib/plugins/gcLoadingMessage.js" type="text/javascript"></script>
-		<script src="../resources/jslib/plugins/gcNavigation.js" type="text/javascript"></script>
-		<script src="../resources/jslib/plugins/gcMeasure.js" type="text/javascript"></script>
-		<script src="../resources/jslib/plugins/gcCoordinates.js" type="text/javascript"></script>
-		<script src="../resources/jslib/plugins/gcMapQuery.js" type="text/javascript"></script>
-		<script src="../resources/jslib/plugins/gcFeaturePopup.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/plugins/gcLoadingMessage.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/plugins/gcNavigation.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/plugins/gcMeasure.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/plugins/gcCoordinates.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/plugins/gcMapQuery.js" type="text/javascript"></script>
+		<script src="../ext-resources/jslib/plugins/gcFeaturePopup.js" type="text/javascript"></script>
 
 		
 <script type="text/javascript">
@@ -55,7 +55,7 @@ $objMapset = new gcMap($_REQUEST["mapset"]);
 
 <?php echo $objMapset->OLMap();?>
 
-Ext.BLANK_IMAGE_URL = "../resources/images/blank.gif";
+Ext.BLANK_IMAGE_URL = "../ext-resources/images/blank.gif";
 Ext.MessageBox.buttonText = {ok : "OK", cancel : "Annulla", yes : "Si",no : "No"};
 
 Ext.onReady(function() {
