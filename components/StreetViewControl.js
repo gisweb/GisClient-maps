c4278001afbe91eacb42fbfa2d2a65879934fa58
anakin2212 <matteo.zero@taulogic.it>
2018-03-16 13:19:21 +0100

    Changed layout and button interaction for Streetview

--- components/StreetViewControl.js
+++ components/StreetViewControl.js
@@ -1,12 +1,20 @@
 $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCqG2a5aKkhA4eGamsp8Y4mrdeEqp8Ez9g", function( data, textStatus, jqxhr ) {
   window.GCComponents["Controls"].addControl('control-streetview', function(map){
-    return new OpenLayers.GisClient.streetViewToolbar({
+    return new OpenLayers.GisClient.streetViewViewer({
         gc_id: 'control-streetview',
         map: map,
         createControlMarkup:customCreateControlMarkup,
-        div:document.getElementById("map-toolbar-streetview"),
-        divbtns: "map-toolbar-streetview-btn",
-        autoActivate:false,
+        div: document.getElementById("map-toolbar-streetview"),
+        autoActivate:false
+    });
+  });
+  window.GCComponents["Controls"].addControl('control-streetview-commander', function(map){
+    return new OpenLayers.GisClient.streetViewToolbar({
+        gc_id: 'control-streetview-commander',
+        map: map,
+        createControlMarkup:customCreateControlMarkup,
+        div: document.getElementById("map-toolbar-streetview-commander"),
+        autoActivate:false
     });
   });
 
@@ -14,15 +22,18 @@ $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCqG2a5aKkhA4eGams
   window.GCComponents["SideToolbar.Buttons"].addButton (
     'button-streetview',
     'StreetView',
-    'icon-street',
+    'glyphicon-white glyphicon-road',
     function() {
       if (sidebarPanel.handleEvent || typeof(sidebarPanel.handleEvent) === 'undefined') {
           var ctrl = this.map.getControlsBy('gc_id', 'control-streetview')[0];
+          var commander = this.map.getControlsBy('gc_id', 'control-streetview-commander')[0];
           if (ctrl.active) {
             ctrl.deactivate();
+            commander.deactivate();
             this.deactivate();
           } else {
             ctrl.activate();
+            commander.activate();
             this.activate();
           }
           if (typeof(sidebarPanel.handleEvent) !== 'undefined')
--- resources/jslib/StreetViewToolbar.js
+++ resources/jslib/StreetViewToolbar.js
@@ -1,189 +1,65 @@
-var yOffset = 1;
-var currentRotation = 0;
-var currentPosition;
-var markerLayer = new OpenLayers.Layer.Vector("Navigatore");
-var pointerLayer = new OpenLayers.Layer.Vector("Pointer");
-var marker;
-var pointer;
-var streetview;
-var streetviewService;
-var currentMarkerStyle = {
-    externalGraphic: '../../resources/img/omemino.png',
-    pointRadius: 22,
-    graphicZIndex: 10
-};
-var currentMarkerPointerLineStyle ={
-    externalGraphic: '../../resources/img/pointer.png',
-    pointRadius: 26,
-    rotation: 0,
-    display: "none",
-    graphicZIndex: 1
-};
-var projection;
-
-OpenLayers.GisClient.Click = OpenLayers.Class(OpenLayers.Control, {
-  defaultHandlerOptions: {
-    'single': true,
-    'double': false,
-    'pixelTolerance': 0,
-    'stopSingle': false,
-    'stopDouble': false
-  },
-
-  initialize: function(options) {
-    this.handlerOptions = OpenLayers.Util.extend({}, this.defaultHandlerOptions);
-    OpenLayers.Control.prototype.initialize.apply(this, arguments);
-    this.handler = new OpenLayers.Handler.Click( this, {'click': this.trigger}, this.handlerOptions);
-  },
-
-  trigger: function(e) {
-    hideCursor(e);
-    this.map.events.unregister("mousemove", this.map, displayCursor);
-    this.map.events.unregister("mouseout", this.map, hideCursor);
-    var currLonLat = this.map.getLonLatFromPixel(e.xy);
-    var lonlat = currLonLat.transform(this.map.projection, 'EPSG:4326');
-    var theloc = new google.maps.LatLng(lonlat.lat,lonlat.lon);
-    streetviewService.getPanorama({location: theloc},(theloc.lng() == 8.534108 && theloc.lat() == 44.536001) ?
-      function(result, status) {
-        streetview.setPano('eggZio');
-      } : function(result, status){
-        if (status === 'ZERO_RESULTS') {
-          streetview.setPano('noview');
-        }
-      }
-    );
-    streetview.setPosition(theloc);
-  }
-});
-
-var click = new OpenLayers.GisClient.Click();
-
+var omeminoCtrl = null;
 OpenLayers.GisClient.streetViewToolbar = OpenLayers.Class(OpenLayers.Control.Panel,{
   div: null,
+  divbtns: null,
   map: null,
   initialize: function(options) {
     OpenLayers.Control.Panel.prototype.initialize.apply(this, [options]);
-    currentPosition = new OpenLayers.LonLat(0,0);
-    projection = this.map.projection;
-    pointer = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(currentPosition.lon, currentPosition.lat + yOffset), null, currentMarkerPointerLineStyle);
-    pointer.move(new OpenLayers.Geometry.Point(currentPosition.lon, currentPosition.lat));
-    pointerLayer.addFeatures([pointer]);
-    pointerLayer.setVisibility(false);
-    marker = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(currentPosition.lon, currentPosition.lat + yOffset), null, currentMarkerStyle);
-    markerLayer.addFeatures([marker]);
-    markerLayer.setVisibility(false);
-    
-    this.map.addLayers([pointerLayer, markerLayer]);
-    streetviewService = new google.maps.StreetViewService;
-    streetview = new google.maps.StreetViewPanorama(this.div, {
-      pov: {heading:0, pitch:10},
-    });
-    streetview.registerPanoProvider(this.getGeoWebProvider);
-    streetview.addListener('position_changed', this.manageMarker);
-    streetview.addListener('pov_changed', this.manageMarker);
+    omeminoCtrl = new OpenLayers.Control.Button({
+        type: OpenLayers.Control.TYPE_TOGGLE,
+        active: false,
+        iconclass:"glyphicon-white glyphicon-user",
+        title:"Puntatore StreetView",
+        text:"Attiva/Disattiva Puntatore StreetView",
+        trigger: function() {
+          if(this.active)
+            this.deactivate();
+          else
+            this.activate();
+        },
+        activate: function() {
+          OpenLayers.Control.Button.prototype.activate.call(this);
+          this.map.events.register("mousemove", this.map, displayCursor);
+          this.map.events.register("mouseout", this.map, hideCursor);
+          this.map.currentControl.deactivate();
+          this.map.currentControl = omeminoCtrl;
+          click.activate();
+          this.active = true;
+        },
+        deactivate: function() {
+          OpenLayers.Control.Button.prototype.deactivate.call(this);
+          hideCursor();
+          this.map.events.unregister("mousemove", this.map, displayCursor);
+          this.map.events.unregister("mouseout", this.map, hideCursor);
+          if(this.map.defaultControl !== undefined) {
+            this.map.currentControl = this.map.defaultControl;
+            this.map.currentControl.activate();
+            click.deactivate();
+          }
+          this.active = false;
+        },
+
+      })
+    this.addControls([omeminoCtrl]);
   },
   draw: function() {
     OpenLayers.Control.Panel.prototype.draw.apply(this);
-    return this.div;
+    return this.div
   },
   redraw: function() {
     OpenLayers.Control.Panel.prototype.redraw.apply(this);
   },
   activate: function() {
-    var activated = OpenLayers.Control.prototype.activate.call(this);
-    if(activated) {
-      this.map.events.register("mousemove", this.map, displayCursor);
-      this.map.events.register("mouseout", this.map, hideCursor);
-      markerLayer.setVisibility(true);
-      pointerLayer.setVisibility(true);
-      this.map.addControl(click);
-      click.activate();
-      $("#"+ this.div.id).addClass("openedStreetView");//="width: 250px; height: 250px;"
-      google.maps.event.trigger(streetview, 'resize');
-    }
+    activated = OpenLayers.Control.prototype.activate.call(this);
+    omeminoCtrl.activate();
+    this.redraw();
   },
   deactivate: function() {
-    var deactivated = OpenLayers.Control.prototype.deactivate.call(this);
-    if(deactivated) {
-      this.map.events.unregister("mousemove", this.map, displayCursor);
-      this.map.events.unregister("mouseout", this.map, hideCursor);
-      markerLayer.setVisibility(false);
-      pointerLayer.setVisibility(false);
-      click.deactivate();
-      this.map.removeControl(click);
-      $("#"+ this.div.id).removeClass("openedStreetView");
-    }
-  },
-  getGeoWebProvider: function(pano) {
-    if (pano === 'eggZio') {
-      marker.style["externalGraphic"] = '../../resources/img/pericolo.png';
-      markerLayer.redraw();
-      return {
-        location: {
-          pano: 'eggZio',
-          description: 'La tana dello zio - Easter Egg',
-          latLng: new google.maps.LatLng(44.536001, 8.534108),
-        },
-        links: [],
-        copyright: 'ZioSim (c) 2018 Iren',
-        tiles: {
-          tileSize: new google.maps.Size(500, 500),
-          worldSize: new google.maps.Size(500, 500),
-          getTileUrl: getEggZioPanoramaTileUrl
-        },
-        sensor: false
-      };
-    } else if(pano === 'noview') {
-      marker.style["externalGraphic"] = '../../resources/img/notfound.png';
-      markerLayer.redraw();
-      return {
-        location: {
-          pano: 'noview',
-          description: 'Immagine non disponibile',
-          latLng: streetview.getPosition()
-        },
-        links: [],
-        tiles: {
-          tileSize: new google.maps.Size(500, 500),
-          worldSize: new google.maps.Size(500, 500),
-          getTileUrl: getNoViewPanoramaTileUrl
-        }
-      };
-    }
-    marker.style["externalGraphic"] = '../../resources/img/omemino.png';
-    markerLayer.redraw();
-    return null;
-  },
-  manageMarker: function() {
-    var currLonLat = streetview.getPosition();
-    var angle = streetview.getPov().heading;
-    var lonLatPointer = new OpenLayers.LonLat(currLonLat.lng(),currLonLat.lat()).transform('EPSG:4326', projection);
-    if(lonLatPointer.lat != currentPosition.lat || currentPosition.lon != lonLatPointer.lon) {
-      var lonLatFeature = new OpenLayers.LonLat(currLonLat.lng(),currLonLat.lat()).transform('EPSG:4326', projection);
-      lonLatFeature.lat += yOffset;
-      pointer.move(lonLatPointer);
-      marker.move(lonLatFeature);
-      currentPosition = lonLatPointer;
-    }
-    pointer.style.rotation = angle;
-    markerLayer.redraw();
-    pointerLayer.redraw();
-  },
-  zoomEnd: function(passedZoom) {
-    if(this.active) {
-      pointer.style.display = passedZoom < this.map.getNumZoomLevels() - 4 ? "none" : "";
-      pointerLayer.redraw();
-    }
+    OpenLayers.Control.prototype.deactivate.call(this);
+    omeminoCtrl.deactivate();
+    this.redraw();
   }
 });
-
-function getEggZioPanoramaTileUrl(pano, zoom, tileX, tileY) {
-  return "../../resources/img/easter.png";
-}
-
-function getNoViewPanoramaTileUrl(pano, zoom, tileX, tileY) {
-  return "";
-}
 
 function displayCursor(e) {
   OpenLayers.Util.getElement("tooltip").innerHTML = "<img src=\"../../resources/img/omemino_fly.png\" alt=\"puntatore\" width=\"25px\" height=\"25px\">";
