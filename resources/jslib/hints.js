e7ca2b7ed53a889ed85307ecdc69459bd1932308
anakin2212 <matteo.zero@taulogic.it>
2018-03-22 10:11:23 +0100

    Added hints div and hints management

---
+++ resources/jslib/hints.js
@@ -0,0 +1,59 @@
+/**
+Script che provvede al caricamento/gestione dei suggerimenti da parte dell'utente
+**/
+$(document).ready(function() {
+  //chiamata ajax per lista file suggerimento
+  $.post(GisClientMap.baseUrl + "/services/listHints.php", { app: HINTS_KEY},
+    function(returnedData){
+      if($.trim(returnedData)) {
+        $("#hintsDivContent").append(returnedData);
+        var show = false;
+        $("#hintsDivContent > div").each(function() {
+          if(localStorage.getItem(HINTS_KEY+"."+this.id)) {
+            $(this).css("display", "none");
+          } else {
+            show = true;
+          }
+        });
+        $("#mapset-question").css("display", "none");
+        $("#hintsDivContent > div > :checkbox").on("click", checkHintCheckbox);
+        if(show) {
+          $("#hintsButton").css("display", "none");
+          $("#hintsDiv").removeClass("hide");
+          setTimeout(function(){ closeHints(); }, 10000);
+        }
+      }
+  });
+  
+
+  
+  $("#mapset-question").on("click", function() {
+    $('#mapset-question').css("display", "none");
+    $("#hintsDiv").show();
+  });
+  $("#hintsButton").on("click", function() {
+    closeHints();
+  });
+});
+
+function checkBoxManagement(arg) {
+  if(localStorage.getItem(HINTS_KEY+"."+arg))
+    localStorage.removeItem(HINTS_KEY+"."+arg);
+  else
+    localStorage.setItem(HINTS_KEY+"."+arg, arg);
+}
+
+function closeHints() {
+ $('#mapset-question').css("display", "");
+ $("#hintsButton").css("display", "");
+ $("#hintsDiv").hide();
+}
+
+function checkHintCheckbox() {
+  var num = $("#hintsDivContent > div > :checkbox").length;
+  var numChk = $("#hintsDivContent > div > :checkbox:checked").length;
+  if(num == numChk) {
+    closeHints();
+    $('#mapset-question').css("display", "none");
+  }
+}
